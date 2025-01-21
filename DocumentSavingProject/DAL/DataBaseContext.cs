4e0c74e20ffc6e60041b31173f863eaec2df4fcf
CEDEPP\utenteprova2 <rostik@cedepp.it>
2025-01-21 12:25:01 +0100

    Initial commit

---
+++ DocumentSavingProject/DAL/DataBaseContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class DataBaseContext:DbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options) { }
+
+        //public DbSet<Notification> Notifications { get; set; }
+
+        //public DbSet<User> Users { get; set; }
+
+        //public DbSet<NotificationType> NotificationTypes { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.ApplyConfigurationsFromAssembly(typeof(NotificationDbContext).Assembly);
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+}
